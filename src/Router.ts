e81ab82f59d7ad1c7a907efb160f543b1b1e6569
Jhon Urueña <jhonf.2703@hotmail.com>
2023-05-13 15:56:59 -0500

    v 0.0.0.2

---
+++ src/Router.ts
@@ -0,0 +1,39 @@
+import express,{Application, Request, Response} from 'express'
+
+class App{
+    
+    //Atributos declarados de typo any
+    public app: any
+    private server: any
+
+    //Crear el constructor
+    constructor(){
+        this.app = express()
+        this.app.use(express.json())
+        this.routes()
+        
+    }
+
+    //Crear metodo de rutas
+    private routes():void{
+        this.app.get( "/",(req:Request,res:Response)=>{
+                res.send("Hola con TypeScript")
+            })
+    }
+
+    //  Metodo para iniciar el server
+    public start():void{
+        this.server = this.app.listen(3000, ()=>{
+            console.log("El servidor esta escuchando en el puerto 3000")
+        })
+    }
+
+    //Metodo para cerrar el server
+    public close():void{
+        this.server.close()
+    }
+
+}
+
+//Exportar la clase App para usarla en otros sitos
+export default App
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import App from "./Router";
+
+//Crer objeto de la clase App
+
+const app = new App()
+app.start()
